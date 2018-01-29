8f096888460a65686bf33d66467cc10f2a38b195
liuylin <1272894923@qq.com>
2018-01-29 15:43:39 +0800

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include"hashtable.h"
+#include"HashLink.h"
+//#include<hash_set>
+
+
+//void TestHashSet()
+//{
+//	hash_set<int> hs;
+//	hs.insert(54);
+//	hs.insert(20);
+//	hs.insert(1);
+//	hs.insert(78);
+//	hs.insert(15);
+//	hs.insert(11);
+//	hs.insert(40);
+//	hs.insert(36);
+//	hs.insert(28);
+//	hs.insert(30);
+//}
+//
+//int main()
+//{
+//	TestHashSet();
+//	system("pause");
+//	return 0;
+//}
+
+int main()
+{
+	TestHashTable();
+	system("pause");
+	return 0;
+}
